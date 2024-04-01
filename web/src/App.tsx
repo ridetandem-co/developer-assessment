a6a9d9f7a499688af751730cac079e7a5c0cf9d0
Butrint Termkolli <butrint.t@icloud.com>
2024-04-01 18:30:01 +0100

    Add initial fetch functionality for bus times

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,32 @@
-interface Props {}
+import React, { useState, useEffect } from "react";
 
-const App: React.FC<Props> = () => {
+interface BusTime {
+  id: number;
+  busId: number;
+  destination: string;
+  minutesUntilArrival: number;
+}
+
+const App: React.FC = () => {
+  const [busTimes, setBusTimes] = useState<BusTime[]>([]);
+
+  useEffect(() => {
+    const fetchBusTimes = async () => {
+      try {
+        const response = await fetch("https://localhost:3000/bus-times");
+        if (!response.ok) {
+          throw new Error("Failed to fetch bus times from API");
+        }
+
+        const data = await response.json();
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchBusTimes();
+  }, []);
+
   return (
     <div className="App">
       <div>
@@ -8,13 +34,17 @@ const App: React.FC<Props> = () => {
           Live bus times for <b>Park Road</b>
         </div>
         <div className="Card">
-          <div className="Card__Header">
-            <b>176</b>
-          </div>
-          <div className="Card__Details">
-            <div>To Newham Close</div>
-            <div>2 mins</div>
-          </div>
+          {busTimes.map((bus) => (
+            <div className="Card__Item" key={bus.id}>
+              <div className="Card__Header">
+                <b>{bus.busId}</b>
+              </div>
+              <div className="Card__Details">
+                <div>To {bus.destination}</div>
+                <div>{bus.minutesUntilArrival} mins</div>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
